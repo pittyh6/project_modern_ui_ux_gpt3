2b2dbc1416f2abdfc2a1125775513ce97f935102
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2023-02-06 15:40:11 +1100

    css files style variables and download assets

--- gpt3_pitty/src/components/navbar/Navbar.jsx
+++ gpt3_pitty/src/components/navbar/Navbar.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import './navbar.css'
+import React from "react";
+import { RiMenu3Line, RiCloneLin } from "react-icons/ri";
+import "./navbar.css";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
