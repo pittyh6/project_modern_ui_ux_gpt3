8d9ed2f67446684f95309c3f11b055ac5c7f5689
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2023-02-12 15:08:24 +1100

    css gpt3__navbar

--- gpt3_pitty/src/components/navbar/Navbar.jsx
+++ gpt3_pitty/src/components/navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { RiMenu3Line, RiCloneLin } from "react-icons/ri";
 import "./navbar.css";
+import logo from '../../assets/logo.svg';
 
 const Navbar = () => {
   return (
